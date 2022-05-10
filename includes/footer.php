ffd29088318c82813deb5fc99272e783fce12565
Priyanka Pathak <pathakpriyanka7624@gmail.com>
2022-05-10 16:29:51 +0545

    first commit

---
+++ includes/footer.php
@@ -0,0 +1,43 @@
+<div class="footer">
+            <div class="container">
+                <div class="row">
+                    <div class="col-lg-6 col-md-6">
+                        <div class="footer-contact">
+                            <h2>Get In Touch</h2>
+                            <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d882.8369052511525!2d85.3239962292039!3d27.737422098923737!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x39eb193b28b9cb6f%3A0xe61b9cca783e028!2z4KSs4KS24KWB4KSo4KWN4KSn4KSw4KS-IOCkmuCkleCljeCksOCkquCkpSDgpJ_gpY3gpLDgpL7gpJUg4KSm4KSV4KWN4KS34KS_4KSjLCBLYXRobWFuZHUgNDQ2MDA!5e0!3m2!1sen!2snp!4v1652078022481!5m2!1sen!2snp" width="350" height="300" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
+                            <div class="footer-social">
+                                <a class="btn" href=""><i class="fab fa-twitter"></i></a>
+                                <a class="btn" href=""><i class="fab fa-facebook-f"></i></a>
+                                <a class="btn" href=""><i class="fab fa-youtube"></i></a>
+                                <a class="btn" href=""><i class="fab fa-instagram"></i></a>
+                                <a class="btn" href=""><i class="fab fa-linkedin-in"></i></a>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-lg-5 col-md-6">
+                        <div class="footer-link">
+                            <h2>Popular Links</h2>
+                              <a href="index.php">Home</a>
+                            <a href="about.php">About Us</a>
+                            <a href="washing-plans.php">Washing Plans</a>
+                            <a href="location.php">Washing Points</a>
+                            <a href="contact.php">Contact Us</a>
+                          
+                            
+              
+                        </div>
+                    </div>
+             
+                </div>
+            </div>
+            <div class="container copyright">
+            <p>&copy; 2022 Clean Sweep</p>
+            </div>
+        </div>
+        <!-- Footer End -->        <!-- Back to top button -->
+        <a href="#" class="back-to-top"><i class="fa fa-chevron-up"></i></a>
+        
+        <!-- Pre Loader -->
+        <div id="loader" class="show">
+            <div class="loader"></div>
+        </div>
